2b213bf36687c4f56cf0a61da68be20af938f785
vplecas <veljkicc110@gmail.com>
2024-02-04 16:41:17 +0100

    first commit

---
+++ src/main/java/com/xm/cryptorecommendationservice/CryptoRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xm.cryptorecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRecommendationServiceApplication.class, args);
+	}
+
+}
